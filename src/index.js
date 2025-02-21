bc2a03e85135d6c62aa21f48155a0ffd037ae544
PrashantVashisth <prashantvashisth2004@gmail.com>
2025-02-21 15:56:49 +0530

    Added logic for message queues

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const setupAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+    
     app.use('/api', apiRoutes);
 
     app.listen(PORT , () => {
